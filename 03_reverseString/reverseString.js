aaa1f74fc78b70fd37f9fa307ad329b8c59f4906
Samir <samirmohd246@gmail.com>
2023-08-21 12:44:02 +0400

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let returnString = "";
+    let length = string.length - 1;
 
+    for (let i = length; i >= 0; i--) {
+        returnString = returnString + string.charAt(i);
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
