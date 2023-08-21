dfabb75370911820f1070131030bee6886b9db39
Samir <samirmohd246@gmail.com>
2023-08-21 12:36:06 +0400

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfTimes) {
+    if (numberOfTimes < 0) {
+        return "ERROR";
+    }
+    
+    let returnString = "";
+    for (let i = 0; i < numberOfTimes; i++) {
+        returnString = returnString + string;
+    }
 
+    return returnString;
 };
 
 // Do not edit below this line
