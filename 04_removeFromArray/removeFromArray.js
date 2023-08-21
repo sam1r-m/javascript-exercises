455b494c464f6b7dc4fac543419e656b8efcdfe4
Samir <samirmohd246@gmail.com>
2023-08-21 15:59:09 +0400

    Solve 04_reverseString

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeTheseValues) {
+    let arrayLength = array.length;
+    let lengthOfValues = removeTheseValues.length;
 
+    for (let i = 0; i < lengthOfValues; i++) {
+        for (let j = 0; j < arrayLength; j++) {
+            if (array[j] === removeTheseValues[i]) {
+                array.splice(j, 1);
+            } 
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
