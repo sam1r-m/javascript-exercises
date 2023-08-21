5567cf5eeb3d51d8caa7598db1142c73f9871ae6
Samir <samirmohd246@gmail.com>
2023-08-21 21:40:34 +0400

    Write 06_leapYears more succintly

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,7 @@
 const leapYears = function(year) {
-    if (((year % 100 == 0) && (year % 400 == 0)) ||
-        ((year % 100 != 0) && (year % 4 == 0))) {
+    if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))) {
         return true;
-    }
+    } 
     
     return false;
 };
