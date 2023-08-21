d926602f292ba24b22228a8f6086ec10dad043b1
Samir <samirmohd246@gmail.com>
2023-08-21 17:22:26 +0400

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let sum = 0;
 
+    if ((typeof(first) != "number") || (typeof(last) != "number")) {
+        return "ERROR";
+    }
+    else if ((first < 0) || (last < 0)) {
+        return "ERROR";
+    }
+    else if (first > last) {
+        let temp = first;
+        first = last;
+        last = temp;
+    }
+
+    for (first; first <= last; first++) {
+        sum = sum + first;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
