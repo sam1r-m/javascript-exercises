f46623bd053f61ede6ba49b63e508a8714fca6d7
Samir <samirmohd246@gmail.com>
2023-08-31 22:57:10 +0400

    Solve 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	arr.forEach((e) => {
+    total += e;
+  })
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if (arr[0] === 0) {
+    return 0;
+  }
+  
+  let total = 1;
+  arr.forEach((e) => {
+    total *= e;
+  })
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0 || a === 1) {
+    return 1;
+  }
+
+  return (a * factorial(a - 1));
 };
 
 // Do not edit below this line
