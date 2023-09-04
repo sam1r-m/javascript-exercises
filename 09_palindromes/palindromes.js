f8f39503207a268b6c80c18bd26c9f8f423ac14e
Samir <samirmohd246@gmail.com>
2023-09-04 11:15:32 +0400

    Solve 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const filteredStringArray = string.split('').filter(e => {
+        return (e !== ' ' && e !== ',' && e !== '.' && 
+                e !== '!' && e !== '?' && e !== '-');
+    })
 
+    let filteredString = filteredStringArray.join('').split(' ').join('');
+    filteredString = filteredString.toLowerCase();
+    const reversedString = filteredString.split('').reverse().join('');
+
+    return (filteredString === reversedString);
 };
 
 // Do not edit below this line
